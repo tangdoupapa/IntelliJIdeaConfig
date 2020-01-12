1046acbebf128a694021d3008fc67788f133ddec
felix <fanyaoxuan@outlook.com>
2020-01-12 21:20:33 +0800

    IU-2019.2.1 <fanya@DESKTOP-CJ48UDR Create baseRefactoring.xml, markdown.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, colors.scheme.xml, github.xml, mavenVersion.xml, templates.xml, AnnotationType.java, MyTemplates.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, MyBatisCodeHelper.xml, proxy.settings.xml, vcs.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,15 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+import java.io.Serializable;
+
+/**
+ *  @author: tangdoupapa
+ *  @Date: ${DATE}
+ *  @Description:
+ *  @Version: V1.0
+ */
+public class ${NAME} implements Serializable {
+
+    private static final long serialVersionUID = -1L;
+    
+}
